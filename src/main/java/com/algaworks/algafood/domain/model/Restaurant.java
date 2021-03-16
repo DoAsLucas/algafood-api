7fc869eefdf03508c6a8c75754d20a7bdb5b9427
Lucas Bispo <lucasbispo_@live.com>
2021-03-16 00:21:09 -0300

    Adds new Model classes
    Adds Kitchen and Restaurant model classes

---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurant.java
@@ -0,0 +1,68 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurant {
+	@Id
+	private Long id;
+
+	@Column
+	private String name;
+
+	@Column(name = "taxa_frete")
+	private BigDecimal taxaFrete;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getTaxaFrete() {
+		return taxaFrete;
+	}
+
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurant other = (Restaurant) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
