0b184cb768ace9e9c9e9be9fbc21e94dee173633
Lucas Bispo <lucasbispo_@live.com>
2021-03-18 00:24:45 -0300

    Inserts Restaurants on application start up

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into kitchen (name) values ('Tailandesa');
 insert into kitchen (name) values ('Indiana');
+
+
+insert into restaurant (name, taxa_frete) values ('Lanchonete Jóia', 7.90);
+insert into restaurant (name, taxa_frete) values ('César Lanches', 9.00);
