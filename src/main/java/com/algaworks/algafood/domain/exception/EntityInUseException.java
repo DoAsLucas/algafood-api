7a1be9e7072b6aa85349dd4d5ef5927bd6d057a3
Lucas Bispo <lucasbispo_@live.com>
2021-03-27 00:49:41 -0300

    Adds exception handling for Delete methods

---
+++ src/main/java/com/algaworks/algafood/domain/exception/EntityInUseException.java
@@ -0,0 +1,10 @@
+package com.algaworks.algafood.domain.exception;
+
+public class EntityInUseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntityInUseException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/algaworks/algafood/domain/service/KitchenService.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.domain.service;
+
+
+import com.algaworks.algafood.domain.exception.EntityInUseException;
+import com.algaworks.algafood.domain.exception.EntityNotFoundException;
+import com.algaworks.algafood.domain.model.Kitchen;
+import com.algaworks.algafood.domain.repository.KitchenRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KitchenService {
+
+	@Autowired
+	KitchenRepository kitchenRepository;
+
+	public Kitchen save(Kitchen kitchen) {
+		return kitchenRepository.save(kitchen);
+	}
+
+	public void delete(Long kitchenId) {
+		try {
+			kitchenRepository.delete(kitchenId);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntityNotFoundException(
+					String.format("There is no kitchen register with ID %d", kitchenId));
+
+		} catch (DataIntegrityViolationException e) {
+			throw new EntityInUseException(
+					String.format("Kitchen which ID is %d cannot be removed, because it is in use", kitchenId));
+		}
+	}
+}
